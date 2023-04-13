b1bdbaddce38d8f279d6cc56193e02dab2833110
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-04-13 17:33:20 -0300

    tratando erros e exceções

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function calculator(){
-    const calculatorKeys = document.querySelectorAll('.calculatorKeys > .numbers, .calculatorKeys > .operators, .calculatorKeys > .decimal')
+    const calculatorKeys = document.querySelectorAll('.calculatorKeys > .numbers, .calculatorKeys > .operators')
 
     const calculatorDisplay = document.querySelector('.calculatorDisplay')
 
@@ -8,32 +8,47 @@ function calculator(){
         // FUNÇÃO ADICIONAR ELEMENTO AO DISPLAY
         function clickKey(event){
             const regexFirstNumber = /^[0-9.]$/
-            const regexOperator = /^[\+\-X\/]$/
+            const regexOperator = /^[\+\-\/X\.]$/
+
             if (calculatorDisplay.innerText == '0'){
                 if (regexFirstNumber.test(event.currentTarget.innerText)){ // VERIFICA SE O PRIMEIRO ELEMENTO DIGITADO É UM NÚMERO DE 0 A 9 OU UM PONTO "."
-                    if (event.currentTarget.innerText == "."){
-                        calculatorDisplay.innerText = "0."
-                    }else{
-                        calculatorDisplay.innerText = event.currentTarget.innerText
-                    }
+                    calculatorDisplay.innerText = event.currentTarget.innerText
                 }
             }else{
-                if (!(regexOperator.test(event.currentTarget.innerText) == numberPrevious)){
+                if (regexOperator.test(calculatorDisplay.innerText.slice(-1)) && regexOperator.test(event.currentTarget.innerText)){
+                    return
+                }else{
                     calculatorDisplay.innerText += event.currentTarget.innerText
                 }
             }
             
-            var numberPrevious = regexOperator.test(event.currentTarget.innerText)
         }
 
         calculatorKeys.forEach((key) => {
             key.addEventListener('click', clickKey)
         })
 
+        // FUNÇÃO ADICIONAR "."
+        function addDecimal(event){
+            const displayTextCopy = calculatorDisplay.innerText
+          
+            if (calculatorDisplay.innerText.slice(-1) == '' || /[+\-/*]/.test(calculatorDisplay.innerText.slice(-1))){
+                calculatorDisplay.innerText += "0."
+            }else if (calculatorDisplay.innerText == "0"){
+                calculatorDisplay.innerText = "0."
+            }else if (/\..*?\./.test(calculatorDisplay.innerText) || /\.{2,}/.test(calculatorDisplay.innerText)){
+                return
+            }else{
+                calculatorDisplay.innerText += "."
+            }
+        }
+
+        const decimal = document.querySelector('.calculatorKeys > .decimal')
+        decimal.addEventListener('click', addDecimal)
 
         // FUNÇÃO MOSTRAR RESULTADO 
         function calculatorResult(){
-            calculatorDisplay.innerText = eval(calculatorDisplay.innerText.replace("X", "*"))
+            calculatorDisplay.innerText = eval(calculatorDisplay.innerText.replace(/X/g, "*"))
         }
 
         const equalButton = document.querySelector('.equal')
